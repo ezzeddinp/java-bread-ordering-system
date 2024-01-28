5189a3c8fb69c1485fc6971bdfeafc145630eeac
Mohammad Ezzeddin Pratama <87559497+ezzeddinp@users.noreply.github.com>
2024-01-28 13:24:22 +0700

    Create Bread.java
    Bread Storage

---
+++ Bread.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Bread {
+	String id;
+	String name;
+	String type;
+	double price;
+
+	public Bread(String id, String name, String type, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.price = price;
+	}
+}
+
